2116b0f66dfde3197684f7ea78250d43c024cd71
allinthemind66 <r.nilon92@gmail.com>
2018-06-27 17:17:44 -0400

    create functions to render movies to movies page

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
-  console.log('in movies page')
+  let moviesList = document.getElementById("moviesList");
+
+  fetch('http://localhost:3000/api/v1/movies/')
+  .then(resp => resp.json())
+  .then(json => renderMoviesHTML(json))
+
+  const renderMoviesToPage = (html) => {
+    moviesList.innerHTML = html;
+  };
+
+  const renderMoviesHTML = (json) => {
+    let htmlBlock = ''
+    json.forEach(movie => {
+      htmlBlock += `<a><li>${movie.title}</li></a>`
+    });
+    renderMoviesToPage(htmlBlock)
+  };
+
 })
